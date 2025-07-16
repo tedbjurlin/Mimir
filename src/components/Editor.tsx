a7b6473bc71692914c54738d9f2bf3debb9637d1
Ted Bjurlin <teddybjurlin@proton.me>
2025-07-16 10:24:09 -0500

    added qol extensions to editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,9 +1,11 @@
-import ReactCodeMirror from "@uiw/react-codemirror";
+import ReactCodeMirror, { drawSelection, EditorView, highlightActiveLine, keymap, rectangularSelection } from "@uiw/react-codemirror";
 import { SetStateAction, useCallback, useState } from "react";
 import { basicDark } from "@uiw/codemirror-theme-basic";
 import { Box } from "@chakra-ui/react";
 import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
 import { languages } from '@codemirror/language-data';
+import { defaultKeymap, history, historyKeymap, indentWithTab } from '@codemirror/commands';
+import { indentOnInput } from "@codemirror/language";
 
 const Editor: React.FC = () => {
   const [value, setValue] = useState("console.log('hello world!');");
@@ -24,6 +26,13 @@ const Editor: React.FC = () => {
             base: markdownLanguage,
             codeLanguages: languages
           }),
+          EditorView.lineWrapping,
+          history(),
+          drawSelection(),
+          rectangularSelection(),
+          highlightActiveLine(),
+          indentOnInput(),
+          keymap.of([indentWithTab, ...defaultKeymap, ...historyKeymap])
         ]}
       />
     </Box>
