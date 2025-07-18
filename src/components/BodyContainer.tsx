f86cd767949102fd5c0ca75428f077f3f788220a
Ted Bjurlin <teddybjurlin@proton.me>
2025-07-18 11:46:38 -0500

    updated allotment

--- src/components/BodyContainer.tsx
+++ src/components/BodyContainer.tsx
@@ -1,18 +1,42 @@
-import Sidedock from "./Sidedock";
-import { Allotment } from "allotment";
+import { Allotment, AllotmentHandle, LayoutPriority } from "allotment";
 import "allotment/dist/style.css";
+import Editor from "./Editor";
+import { useRef, useState } from "react";
 
 const BodyContainer: React.FC = () => {
+  const ref = useRef<AllotmentHandle | null>(null);
+
+  const [leftVisible, setLeftVisible] = useState(true);
+  const [rightVisible, setRightVisible] = useState(true);
 
   return (
-    <Allotment defaultSizes={[100, 200]} minSize={70}>
-      <div className="bg-green-600 h-dvh" />
-      <div className="bg-red-600 h-dvh" />
-      <Allotment.Pane snap>
-        <div className="bg-blue-600 h-dvh" />
+    <Allotment
+      defaultSizes={[100, 200]}
+      minSize={0}
+      proportionalLayout={false}
+      ref={ref}
+    >
+      <Allotment.Pane
+        snap
+        maxSize={200}
+        minSize={100}
+        priority={LayoutPriority.Low}
+      >
+        <div>Left Leaf</div>
+      </Allotment.Pane>
+      <Allotment.Pane priority={LayoutPriority.Normal}>
+        <Editor />
+      </Allotment.Pane>
+      <Allotment.Pane
+        snap
+        maxSize={200}
+        minSize={100}
+        priority={LayoutPriority.Low}
+      >
+        <div>Right Leaf</div>
       </Allotment.Pane>
     </Allotment>
   );
-}
+};
 
 export default BodyContainer;
