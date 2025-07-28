7efd0fd04be8d90667c404d504f59efd44c6746f
Ted Bjurlin <teddybjurlin@proton.me>
2025-07-28 14:23:57 -0500

    added classes to tab component

--- src/components/tabs/Tab.tsx
+++ src/components/tabs/Tab.tsx
@@ -1,18 +1,19 @@
-import { LuX } from "react-icons/lu";
+import { LuFile, LuX } from "react-icons/lu";
+import { TabState } from "./TabReducer";
 
 type TabProps = {
-  uuid: `${string}-${string}-${string}-${string}-${string}`;
-  name: string;
-  icon: React.ReactNode;
+  state: TabState;
 };
 
-const Tab: React.FC<TabProps> = ({ uuid, name, icon }) => {
+const Tab: React.FC<TabProps> = ({ state }) => {
   return (
-    <div>
-      {icon}
-      {name}
-      <button>
-        <LuX />
+    <div
+      className={`tab ${state.selected ? "tab--selected" : "tab--unselected"}`}
+    >
+      <LuFile className="tab__icon" />
+      {state.temporary ? <i>state.title</i> : state.title}
+      <button className="tab__close-button">
+        <LuX className="tab_close-icon" />
       </button>
     </div>
   );
