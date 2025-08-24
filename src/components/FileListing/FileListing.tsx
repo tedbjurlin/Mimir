3664bf87b97b26e4e41e11666dd10ef51a5ebd1c
Ted Bjurlin <teddybjurlin@proton.me>
2025-08-24 10:32:28 -0500

    refactored around new dir structure

--- src/components/FileListing/FileListing.tsx
+++ src/components/FileListing/FileListing.tsx
@@ -5,8 +5,6 @@ import { ChevronRightIcon, FileIcon } from "lucide-react";
 import { useContext, useEffect, useState } from "react";
 import { AppStateDispatchContext } from "@/components/AppStateProvider";
 import { invoke } from "@tauri-apps/api/core";
-import Database from "@tauri-apps/plugin-sql";
-import { debug } from "@tauri-apps/plugin-log";
 
 const textExtensions = ["txt", "md", "typ"];
 
